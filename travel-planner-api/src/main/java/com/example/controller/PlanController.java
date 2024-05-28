b381a7ceee17d053e734b766a2732be6ce4a649c
haemiii <eham1003@naver.com>
2024-05-28 17:04:01 +0900

    Tread-safe를 위한 ConcurrenthashMap, AtomicLong

--- travel-planner-api/src/main/java/com/example/controller/PlanController.java
+++ travel-planner-api/src/main/java/com/example/controller/PlanController.java
@@ -7,20 +7,21 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicLong;
 
 @RestController
 @RequestMapping("/plans")
 public class PlanController {
 
-    private Map<Long, TravelPlan> plans = new HashMap<>();
-    private Long id = 1L;
+    private Map<Long, TravelPlan> plans = new ConcurrentHashMap<>();
+    private AtomicLong id = new AtomicLong(1);
 
     @PostMapping("/create")
     public ResponseEntity<TravelPlan> createPlan(@RequestBody TravelPlan travelPlan){
         travelPlan.setDayPlanList(travelPlan.getStart_date(), travelPlan.getEnd_date());
-
         // travelPlan 저장 로직 추가 예정
-        plans.put(id++, travelPlan);
+        plans.put(id.getAndIncrement(), travelPlan);
         return new ResponseEntity<>(travelPlan, HttpStatus.CREATED);
     }
 

ab26cf56723b2265646352988ec337dbe458ad36
haemiii <eham1003@naver.com>
2024-05-30 22:20:58 +0900

    PlanControllerTest: mockMVc -> TestRestTemplate 로 변경

--- travel-planner-api/src/test/java/com/example/controller/PlanControllerTest.java
+++ travel-planner-api/src/test/java/com/example/controller/PlanControllerTest.java
@@ -6,24 +6,18 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.http.MediaType;
-import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.*;
 
 import java.time.LocalDate;
 
-import static org.hamcrest.Matchers.hasSize;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
-
-@WebMvcTest(PlanController.class)
+import static org.assertj.core.api.Assertions.assertThat;
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class PlanControllerTest {
 
     @Autowired
-    private MockMvc mockMvc;
+    private TestRestTemplate testRestTemplate;
 
     @Autowired
     private ObjectMapper objectMapper;
@@ -42,42 +36,43 @@ class PlanControllerTest {
         secondPlan = new TravelPlan();
         secondPlan.setName("second plan");
         secondPlan.setStartDate(LocalDate.of(2024, 6, 10));
-        secondPlan.setEndDate(LocalDate.of(2024, 6, 12));
+        secondPlan.setEndDate(LocalDate.of(2024, 6, 13));
 
     }
 
     @Test
     @DisplayName("일정 생성 테스트")
     public void testCreatePlan() throws Exception{
-        mockMvc.perform(post("/plans/create")
-                .contentType(MediaType.APPLICATION_JSON)
-                .content(objectMapper.writeValueAsString(firstPlan)))
-                .andExpect(status().isCreated())
-                .andExpect(jsonPath("$.id").exists())
-                .andExpect(jsonPath("$.dayPlanList", hasSize(3)));
+//        HttpHeaders headers = new HttpHeaders();
+//        headers.setContentType(MediaType.APPLICATION_JSON);
+//
+//        HttpEntity<String> request = new HttpEntity<>(objectMapper.writeValueAsString(firstPlan), headers);
+        ResponseEntity<String> response =  testRestTemplate.postForEntity("/plans/create", firstPlan, String.class);
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.CREATED);
+
+        TravelPlan travelPlan = objectMapper.readValue(response.getBody(), TravelPlan.class);
+        assertThat(travelPlan.getId()).isNotNull();
+        assertThat(travelPlan.getDayPlanList()).hasSize(3);
+
     }
 
     @Test
     @DisplayName("Id 값으로 일정 조회")
-    public void testGetPlanById() throws Exception{
-        String first = mockMvc.perform(post("/plans/create")
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content(objectMapper.writeValueAsString(firstPlan)))
-                .andExpect(status().isCreated())
-                .andReturn().getResponse().getContentAsString();
+    public void testGetPlanById() throws Exception {
+        ResponseEntity<TravelPlan> response1 = testRestTemplate.postForEntity("/plans/create", firstPlan, TravelPlan.class);
+        ResponseEntity<TravelPlan> response2 = testRestTemplate.postForEntity("/plans/create", secondPlan, TravelPlan.class);
 
-        String second = mockMvc.perform(post("/plans/create")
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content(objectMapper.writeValueAsString(secondPlan)))
-                .andExpect(status().isCreated())
-                .andReturn().getResponse().getContentAsString();
+        // id값으로 일정조회
+        ResponseEntity<TravelPlan> getResponse1 = testRestTemplate.getForEntity("/plans/{id}", TravelPlan.class, 1);
+        ResponseEntity<TravelPlan> getResponse2 = testRestTemplate.getForEntity("/plans/{id}", TravelPlan.class, 2);
+        assertThat(getResponse1.getStatusCode()).isEqualTo(HttpStatus.OK);
 
-        TravelPlan secondPlan = objectMapper.readValue(second, TravelPlan.class);
+        TravelPlan travelPlan1 = getResponse1.getBody();
+        TravelPlan travelPlan2 = getResponse2.getBody();
 
-        mockMvc.perform(get("/plans/"+secondPlan.getId()))
-                .andExpect(status().isOk())
-                .andExpect(jsonPath("$.name").value("second plan"));
-
-
+        assertThat(travelPlan1.getName()).isEqualTo("first plan");
+        assertThat(travelPlan1.getDayPlanList()).hasSize(3);
+        assertThat(travelPlan2.getName()).isEqualTo("second plan");
+        assertThat(travelPlan2.getDayPlanList()).hasSize(4);
     }
 }
